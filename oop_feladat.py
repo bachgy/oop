3cdb32b7bb32187eb3d9e6c24cdc206a0f64605f
bachgy <bachgy@SYSADMIN10>
2024-04-10 14:50:21 +0200

    Elkészült

--- oop_feladat.py
+++ oop_feladat.py
@@ -1,5 +1,99 @@
 #VZJDIG
-from abc import  ABC
+import re
+import sys
+from datetime import date
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from Design import Ui_MainWindow
+from reservationListWindow import  Ui_reservListWindow
+from reservationAdd import Ui_addReserv
+from ReservationCancel import Ui_ReservCancel
+from abc import ABC
+
+class MainApp(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def slotViewReservs(self):
+        self.Foglalas_listazo = ReservListApp()
+        FoglalasClass = Foglalas
+        for x in FoglalasClass.peldanyok:
+          self.Foglalas_listazo.listWidget.addItem(str(x))
+        self.Foglalas_listazo.show()
+
+    def slotCreateReserv(self):
+        # print("Foglalás készítése")
+        self.Foglalas_keszito = ReservAddApp()
+        SzallodaiSzobak = Szoba
+        for x in SzallodaiSzobak.szobaszam:
+            # print(x)
+            self.Foglalas_keszito.cmbSzobak.addItem(str(x))
+        self.Foglalas_keszito.show()
+
+
+    def slotDeleteReserv(self):
+        self.Foglalas_torlo = reservCancelApp()
+        FoglalasClass = Foglalas
+        for x in FoglalasClass.peldanyok:
+            self.Foglalas_torlo.listWidget.addItem(str(x))
+        self.Foglalas_torlo.show()
+
+class ReservAddApp(QMainWindow, Ui_addReserv):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def slot_tmp(self):
+        kiv_szobaszam = self.cmbSzobak.currentText()
+        SzallodaClass = szalloda
+        for x in SzallodaClass.szobak:
+            if kiv_szobaszam in x:
+                print(x)
+
+    def leiras_megkereses_kivalaszott_szobaszam_alapjan(self):
+        kiv_szobaszam = self.cmbSzobak.currentText()
+        SzallodaClass = szalloda
+        for x in SzallodaClass.szobak:
+            if kiv_szobaszam in x:
+                return (x)
+    def btnClick(self):
+        SzobaFoglalas = Foglalas
+        SzallodaClass = szalloda
+        NemLehetsegesFoglalasok = []
+        for x in Foglalas.peldanyok:
+            NemLehetsegesFoglalasok.append((str(x.datum), str(x.szobaszam)))
+
+        if (self.txtGuestName.toPlainText()) == "":
+            QMessageBox.information(None, "Információ", "Nem töltötte ki a vendég neve mezőt!")
+        else:
+            if (self.calendarWidget.selectedDate().toPyDate()) < date.today():
+                QMessageBox.information(None, "Információ", "A múltba nem lehetséges foglalni!")
+            else:
+                if (str(self.calendarWidget.selectedDate().toPyDate()),self.cmbSzobak.currentText()) in NemLehetsegesFoglalasok:
+                    QMessageBox.information(None, "Információ", "Nem lehetséges, mert ezen a dátumon ebben a szobában már van foglalás")
+                else:
+                    SzobaFoglalas.hozzaad(self.cmbSzobak.currentText(),self.txtGuestName.toPlainText(),str(self.calendarWidget.selectedDate().toPyDate()))
+                    QMessageBox.information(None, "Információ",f"Sikeres foglalás\n" + "A szoba jellemzői:\n" + self.leiras_megkereses_kivalaszott_szobaszam_alapjan())
+                    self.close()
+
+class ReservListApp(QMainWindow, Ui_reservListWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+class reservCancelApp(QMainWindow, Ui_ReservCancel):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def slotCancelReserv(self):
+        FoglalasClass = Foglalas
+        regexKif = r"Sorszám: (\d+)"
+        torlendoElemIndexe = re.search(regexKif,self.listWidget.currentItem().text())
+        print(torlendoElemIndexe.group(1))
+        del FoglalasClass.peldanyok[int(torlendoElemIndexe.group(1))]
+        QMessageBox.information(None, "Információ", "A kiválasztott fogalalás törlése megtörtént!")
+        self.close()
 
 class Szalloda:
     def __init__(self, nev):
@@ -7,24 +101,40 @@ class Szalloda:
         self.szobak = []  # Szobák tárolása
 
     def szoba_hozzaad(self, szoba):
-        self.szobak.append(szoba)
+        self.szobak.append(szoba.leiras())
 
     def __str__(self):
         return f"{self.nev} Szálloda, Szobák száma: {len(self.szobak)}"
 
 class Foglalas:
-    def __init__(self, szoba, vendeg_nev, napok_szama):
-        self.szoba = szoba
-        self.vendeg_nev = vendeg_nev
-        self.napok_szama = napok_szama
 
+    peldanyok = []
+    datumok = []
+    szobak = []
+
+    def __init__(self, szobaszam, vendeg_nev, mely_napra):
+        self.szobaszam = szobaszam
+        self.vendeg_nev = vendeg_nev
+        self.datum = mely_napra
+        Foglalas.peldanyok.append(self)
+        Foglalas.datumok.append(mely_napra)
+        Foglalas.szobak.append(self.szobak)
+        self.index = Foglalas.peldanyok.index(self)
     def __str__(self):
-        return f"Foglalás: {self.vendeg_nev} - {self.szoba.leiras()}, Napok száma: {self.napok_szama}"
+        #return f"Foglalás: {self.vendeg_nev} - {self.szoba.leiras()}, Dátum: {self.datum}"
+        return f"Sorszám: {self.index} Név: {self.vendeg_nev}, Szobaszám: {self.szobaszam}, Dátum: {self.datum}"
+    @classmethod
+    def hozzaad(cls, szoba, vendeg_nev, mely_napra):
+        return cls(szoba, vendeg_nev, mely_napra)
+
 
 class Szoba(ABC):
+
+    szobaszam = []
     def __init__(self, ar, szobaszam):
         self.ar = ar
         self.szobaszam = szobaszam
+        Szoba.szobaszam.append(szobaszam)
 
     def leiras(self):
         pass
@@ -48,19 +158,25 @@ class KetagyasSzoba(Szoba):
     def leiras(self):
         return f"Ketágyas szoba, Szobaszám: {self.szobaszam}, Ár: {self.ar} Ft/éj, {'Reggeli' if self.reggeli else 'Nincs reggeli'}, {'Erkélyes' if self.erkely else 'Nincs erkély'}"
 
-szalloda = Szalloda("Grande Hotel")
+szalloda = Szalloda("Grand Hotel GDE")
 
 egyagyas = EgyagyasSzoba(10000, 101, True,True)
-ketagyas = KetagyasSzoba(18000, 102, False, True)
+ketagyas_1 = KetagyasSzoba(18000, 102, False, True)
+ketagyas_2 = KetagyasSzoba(16000, 201, True, True)
 
 szalloda.szoba_hozzaad(egyagyas)
-szalloda.szoba_hozzaad(ketagyas)
+szalloda.szoba_hozzaad(ketagyas_1)
+szalloda.szoba_hozzaad(ketagyas_2)
 
-foglalas1 = Foglalas(egyagyas, "Kis József", 2)
-foglalas2 = Foglalas(ketagyas, "Nagy Anna", 3)
-foglalas3 = Foglalas(ketagyas, "Nagy Annaaaaa", 1)
+Foglalas.hozzaad(egyagyas.szobaszam, "Nagy Béla", '2024-06-13')
+Foglalas.hozzaad(egyagyas.szobaszam, "teszt", '2024-06-10')
+Foglalas.hozzaad(ketagyas_1.szobaszam, "Kovács Anna", '2024-06-18')
+Foglalas.hozzaad(ketagyas_2.szobaszam, "Teszt Elek", '2024-06-12')
+Foglalas.hozzaad(egyagyas.szobaszam, "Egyágyas István", '2024-06-20')
 
-print(szalloda)
-print(foglalas1)
-print(foglalas2)
-print(foglalas3)
+if __name__ == "__main__":
+     app = QApplication(sys.argv)
+     window = MainApp()
+    # window = ReservAddApp()
+     window.show()
+     sys.exit(app.exec())
